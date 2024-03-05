814b9705c07dfabbd063fdb3b0351746dade043b
Corey11611 <vitalik.itachi123@yandex.ru>
2024-03-05 15:36:00 +0300

    Разработал свой класс и добавил ссылки

--- ClassLibrary/Lesson.cs
+++ ClassLibrary/Lesson.cs
@@ -6,7 +6,25 @@ using System.Threading.Tasks;
 
 namespace ClassLibrary
 {
-    public class Lesson
+    internal class Lesson
     {
+        private string date;
+        private Discipline discipline;
+        private Employee employee;
+        private Classroom classroom;
+        private Group group;
+        private Pair pair;
+        private Body body; /// 14 варианта у нас нет, поэтому ссылаюсь на 13
+
+        public Lesson(string date, Discipline discipline, Employee employee, Classroom classroom, Group group, Pair pair, Body body)
+        {
+            this.date = DateTime.Today.ToString();
+            this.discipline = discipline;
+            this.employee = employee;
+            this.classroom = classroom;
+            this.group = group;
+            this.pair = pair;
+            this.body = body;
+        }
     }
 }
