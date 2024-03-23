751052982ef74f4db38f849b1d1129b51b38a8c7
Andrey281 <malyshev.andreyev@gmail.com>
2024-03-23 09:00:08 +0300

    Переименовал класс

--- ClassLibrary/Job title.cs
+++ ClassLibrary/Job title.cs
@@ -10,14 +10,14 @@ namespace ClassLibrary
     /// https://docs.google.com/document/d/1yBWzXRuxmOQiPQs9TyNx8tNgq6nWF42Sjn-mgT_-5nY/edit
     /// 10 variant
     /// </summary>
-    internal class Job_title
+    internal class JobTitle
     {
         private string name;
         private int salary;
         private Subdivision subdivision;
 
 
-        public Job_title(string name, int salary, Subdivision subdivision)
+        public JobTitle(string name, int salary, Subdivision subdivision)
         {
             this.name = name;
             this.salary = salary;
