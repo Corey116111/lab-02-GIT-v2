a77e36aa699f91198712919e4a492eee87b707ba
SelfKnowledge <s31fknow1edge@gmail.com>
2024-04-05 22:41:56 +0300

    class organisation changes

--- ClassLibrary/Organization.cs
+++ ClassLibrary/Organization.cs
@@ -1,25 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ClassLibrary
 {
-    internal class Organisation
+    public class Organisation
     {
-        private string name;
-        private string legalAdress;
-        private string actualAdress;
-        private Employee employee;
-        public Organisation(string name, string legalAdress, string actualAdress, string refSupervisor)
+        private readonly string name;
+        private readonly string legalAdress;
+        private readonly string actualAdress;
+        private readonly Employee employee;
+        public Organisation(string name, string legalAdress, string actualAdress, Employee employee)
         {
             this.name = name;
             this.legalAdress = legalAdress;
             this.actualAdress = actualAdress;
-            this.employee = employee; 
-
+            this.employee = employee;
         }
+
+        public string Name { get { return name; } }
+        public string LegalAdress { get { return legalAdress; } }
+        public string ActualAdress { get { return actualAdress; } }
+        public Employee Employee { get { return employee; } }
     }
 
 }
