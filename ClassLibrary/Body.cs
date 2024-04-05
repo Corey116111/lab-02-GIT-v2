23299e27dd78a0c7e77a432e607ff4928c73001f
Corey11611 <vitalik.itachi123@yandex.ru>
2024-04-05 23:42:56 +0300

    Поправил

--- ClassLibrary/Body.cs
+++ ClassLibrary/Body.cs
@@ -1,25 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 namespace ClassLibrary
-{
-    internal class Body
+{   /// <summary>
+    /// https://docs.google.com/document/d/13_TXjBoKn0TzKMcWP9A6jX2vyQEgcxBc4vY1CfJn9ps/edit?pli=1 13 вариант
+    /// </summary>
+    public class Body
     {
-        private string name;
-        private string address;
-        private string TheCommandant;
-        private Student student;
-        private Organization organization;
-        public Body(string name, string address, string TheCommandant, Student student, Organization organization)
+        private readonly string name;
+        private readonly string address;
+        private readonly Student student;
+        private readonly Organization organization;
+        public Body(string name, string address,  Student student, Organization organization)
         {
             this.name = name;
             this.address = address;
-            this.TheCommandant = TheCommandant;
             this.student = student;
             this.organization = organization;
         }
+
+        public string Name { get { return name; } }
+        public string Address { get { return address; } }
+        
+        public Student Student => student;
+
+        public Organization Organization => organization;  
+
     }
 }
