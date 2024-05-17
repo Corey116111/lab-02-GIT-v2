1719bfb6ec3e146c3f3276b75c96219d1f9caf75
Андрей Ронжин <ronzhin05@bk.ru>
2024-05-17 22:59:55 +0300

    удалил коменты

--- ClassLibrary/Shift.cs
+++ ClassLibrary/Shift.cs
@@ -7,12 +7,11 @@ using System.Xml.Linq;
 
 namespace ClassLibrary
 {
-<<<<<<< HEAD
+
     /// <summary>
     /// https://docs.google.com/document/d/1QSQdwb3s6IOh4avDr_-vpiYnII_80yUAVL3H_JkLNXQ/edit 8 вариант
     /// </summary>
-=======
->>>>>>> dev
+
     public class Shift
     {
         private string name;
