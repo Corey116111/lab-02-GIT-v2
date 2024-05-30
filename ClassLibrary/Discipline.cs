13121fa9e64231e4daf78feb371f993313bea881
вика <@DESKTOP-0JJ2ITH>
2024-05-30 19:36:21 +0300

    Удалена дата, добавлена ссылка на вариант

--- ClassLibrary/Discipline.cs
+++ ClassLibrary/Discipline.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace ClassLibrary
 {
+    /// <summary>
+    /// https://docs.google.com/document/d/1qCEMkbMRvvHP361IFDeWgSpHfVRUp6BV67nrsjl3fSw/edit 3 вариант
+    /// </summary>
     public class Discipline
     {
         private readonly string name;
