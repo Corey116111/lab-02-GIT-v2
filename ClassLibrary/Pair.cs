80444ad1e525372b44212c24ba3e455035ef92de
Visherrrix <vish20002@gmail.com>
2024-05-30 20:20:22 +0300

    класс изменен

--- ClassLibrary/Pair.cs
+++ ClassLibrary/Pair.cs
@@ -7,23 +7,45 @@ using System.Threading.Tasks;
 
 namespace ClassLibrary
 {
-    internal class Pair
+    /// <summary>
+    /// https://docs.google.com/document/d/1r4OjKXg5_5yj6ibHB778UEF5Z1nbUqGdXjpQXbTGWiQ/edit 7 вариант
+    /// </summary>
+    public class Pair //// люди за сомнительную деятельность тимлида ответственности не несут   ㄟ( ▔, ▔ )ㄏ
     {
-        private readonly DateTime dateHeld;
-        private string time_pair_start;
-        private string time_pair_end;
-        private string time_break_start;
-        private string time_break_end;
-        private Shift shift;
+        private readonly TimeSpan time_pair_start;
+        private readonly TimeSpan time_pair_end;
+        private readonly TimeSpan time_break_start;
+        private readonly TimeSpan time_break_end;
+        private readonly Shift shift;
 
-        public Pair(DateTime dateHeld, string time_pair_start, string time_pair_end, string time_break_start, string time_break_end, Shift shift)
+        public Pair(TimeSpan time_pair_start, TimeSpan time_pair_end, TimeSpan time_break_start, TimeSpan time_break_end, Shift shift)
         {
-            this.dateHeld = dateHeld != default? dateHeld: DateTime.Now;
             this.time_pair_start = time_pair_start;
             this.time_pair_end = time_pair_end;
             this.time_break_start = time_break_start;
             this.time_break_end = time_break_end;
             this.shift = shift;
         }
+        public TimeSpan Time_pair_start
+        {
+            get { return time_pair_start; }
+        }
+
+        public TimeSpan Time_pair_end
+        {
+            get { return time_pair_end; }
+        }
+
+        public TimeSpan Time_break_start
+        {
+            get { return time_break_start; }
+        }
+        public TimeSpan Time_break_end
+        {
+            get { return time_break_end; }
+        }
+
+        public Shift Shift { get { return shift; } }
     }
 }
+
