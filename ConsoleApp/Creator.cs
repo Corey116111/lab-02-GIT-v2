0a7e477111deb439e4949ab84a411437bbdfd7ff
Corey11611 <vitalik.itachi123@yandex.ru>
2024-06-01 11:58:18 +0300

    fix

--- ConsoleApp/Creator.cs
+++ ConsoleApp/Creator.cs
@@ -295,7 +295,7 @@ namespace ConsoleApp
             return null;
         }
 
-        static public Body CreateBody()
+        static public BuildingBody CreateBody()
         {
             Console.WriteLine("Введите название корпуса:");
             string name = Console.ReadLine();
@@ -307,11 +307,11 @@ namespace ConsoleApp
 
             Organization organization = CreateOrganization();
 
-            Body body = DB.bodies.FirstOrDefault(l => employee == l.Employe && organization == l.Organization);
+            BuildingBody body = DB.bodies.FirstOrDefault(l => name == l.Name && organization == l.Organization);
 
             if (body == null)
             {
-                body = new Body(name, address, employee, organization);
+                body = new BuildingBody(name, address, employee, organization);
                 DB.bodies.Add(body);
                 Console.WriteLine("Корпус успешно создан.");
             }
