0fe0bae6ac4feacab95c5fd5615f0ca13671e371
voidnlie <147824582+voidnlie@users.noreply.github.com>
2024-06-01 09:19:11 +0300

    Update and rename Body.cs to BuildingBody.cs

--- ClassLibrary/BuildingBody.cs
+++ ClassLibrary/BuildingBody.cs
@@ -2,31 +2,28 @@
 using System.Text;
 using System.Threading.Tasks;
 namespace ClassLibrary
-<<<<<<< HEAD
+
 {   /// <summary>
     /// https://docs.google.com/document/d/13_TXjBoKn0TzKMcWP9A6jX2vyQEgcxBc4vY1CfJn9ps/edit?pli=1 13 вариант
     /// </summary>
-=======
-{
->>>>>>> dev
-    public class Body
+    public class BuildingBody
     {
         private readonly string name;
         private readonly string address;
-        private readonly Employee employee;
+        private readonly Employee commandant;
         private readonly Organization organization;
 
-        public Body(string name, string address, Employee employee, Organization organization)
+        public BuildingBody(string name, string address, Employee commandant, Organization organization)
         {
             this.name = name;
             this.address = address;
-            this.employee = employee; 
+            this.commandant = commandant; 
             this.organization = organization; 
         }
 
         public string Name { get { return name; } }
         public string Address { get { return address; } }
-        public Employee Employe => employee;
+        public Employee Employe => commandant;
         public Organization Organization => organization;
     }
 }
